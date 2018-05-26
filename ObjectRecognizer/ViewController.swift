a901209335d0d4cf51fc1ccff437209be087f0b2
Vlad Klimenko <vklimenko@smedialink.com>
2018-05-26 13:09:46 +0300

    Initial Commit

---
+++ ObjectRecognizer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ObjectRecognizer
+//
+//  Created by vladislav klimenko on 20/05/2018.
+//  Copyright © 2018 Wooden Co. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+
+
+}
+
